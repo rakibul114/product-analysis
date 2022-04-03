2520f67a95527e83e545a0314028602c286897dd
rakibul114 <rakibulislam114@gmail.com>
2022-04-04 03:26:16 +0600

    top banner added at home page

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,25 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
+
 const Header = () => {
     return (
-      <nav className="bg-slate-300 text-2xl text-center py-6 font-medium">
-        <Link className="nav-link" to="/">
-          Home
-        </Link>
-        <Link className="nav-link" to="reviews">
-          Reviews
-        </Link>
-        <Link className="nav-link" to="/dashboard">
-          Dashboard
-        </Link>
-        <Link className="nav-link" to="/blogs">
-          Blogs
-        </Link>
-        <Link className="nav-link" to="/about">
-          About
-        </Link>
-      </nav>
+      <div>
+        <nav className="bg-slate-300 text-2xl text-center py-8 font-medium">          
+          <Link className="nav-link" to="/">
+            Home
+          </Link>
+          <Link className="nav-link" to="reviews">
+            Reviews
+          </Link>
+          <Link className="nav-link" to="/dashboard">
+            Dashboard
+          </Link>
+          <Link className="nav-link" to="/blogs">
+            Blogs
+          </Link>
+          <Link className="nav-link" to="/about">
+            About
+          </Link>
+        </nav>
+      </div>
     );
 };
 
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import './Home.css';
+import image from '../Assets/top-banner.jpg';
 
 const Home = () => {
     return (
+      <div className="home flex items-center justify-around my-8">
         <div>
-            <h1>This is home</h1>
+          <h1 className="text-6xl font-bold">
+            WE SEE...<br></br>WHAT CAN BE
+          </h1>
+          <p className="text-justify mt-5">
+            Elite Interior Design provides premium interior design services that
+            guarantee results.<br></br> Elite Interior specializes in Home
+            Renovations, New Builds, Decor and Commercial Projects.<br></br> We
+            offer interior design services within all over the country mostly in
+            city areas.
+          </p>
         </div>
+        <img src={image} alt="" />
+      </div>
     );
 };
 

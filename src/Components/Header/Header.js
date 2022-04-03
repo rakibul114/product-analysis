7e39dc7a82005b08fe8a63d6d6e40b892cacc601
rakibul114 <rakibulislam114@gmail.com>
2022-04-04 02:39:07 +0600

    navbar and links are created

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
-        <nav>
-            <h1>This is header</h1>
-        </nav>
+      <nav className="bg-slate-300 text-2xl text-center py-6 font-medium">
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+        <Link className="nav-link" to="reviews">
+          Reviews
+        </Link>
+        <Link className="nav-link" to="/dashboard">
+          Dashboard
+        </Link>
+        <Link className="nav-link" to="/blogs">
+          Blogs
+        </Link>
+        <Link className="nav-link" to="/about">
+          About
+        </Link>
+      </nav>
     );
 };
 

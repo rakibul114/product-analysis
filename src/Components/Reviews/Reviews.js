82561367b5cefbc4f4e8b65848395ba4fb1def52
rakibul114 <rakibulislam114@gmail.com>
2022-04-04 19:46:38 +0600

    review page updated

--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -7,7 +7,7 @@ const Reviews = () => {
     const [services, setServices] = useServices();
     return (
       <div>
-        <h1 className="text-5xl text-center my-5">Interior Design Services</h1>
+        <h1 className="text-5xl text-center my-5">What Our Clients Say...</h1>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-2 justify-items-center">
             {
                 services.map((service) => (
